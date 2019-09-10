7028896e5ebc47037c0b40fd749a8f28b090f878
Kenshex7 <hector12x13@gmail.com>
2019-09-10 16:42:31 -0500

    fahrenheit-celsius

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
-
 /* print Fahrenheit-Celsius table */
 
-int main()
+int main(int argc, char **argv[])
 {
-    int fahr;
-
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-
-    return 0;
+	int fahr;
+	int low, up, inc;
+	if(argc==4){
+		low=atoi(argv[1]);
+		up=atoi(argv[2]);
+		inc=atoi(argv[3]);
+		for (fahr = low; fahr <= up; fahr = fahr + inc)
+			printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+	}
+	if(argc==2){
+		fahr=atoi(argv[1]);
+		printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+	}
+	else if(argc==3 || argc<2 || argc>4){
+		printf("Invalid syntax, you have to use the input like:\n./fahrenheit_celsius lowLimit UpLimit increment\nOr ./fahrenheit_celsius fahrDegreess");
+	}
+	return 0;
 }
+
