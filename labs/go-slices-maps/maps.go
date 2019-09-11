8ad0f455a2ae8478c6e487c7b33c01c576221fd1
Kenshex7 <hector12x13@gmail.com>
2019-09-10 20:41:45 -0500

    Go slices maps

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+  "golang.org/x/tour/wc"
+  "strings"
+)
+
+func WordCount(s string) map[string]int {
+  var stringArray []string = strings.Fields(s)
+  wordMap := make(map[string]int)
+  for _, word := range stringArray {
+    count, ok := wordMap[word]
+    if ok {
+      wordMap[word] = count + 1
+    } else {
+      wordMap[word] = 1
+    }
+  }
+  return wordMap
+}
+
 func main() {
+  wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,21 @@
 package main
 
-func main() {
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	data := make([][]uint8, dy)
+  for i := range data {
+      data[i] = make([]uint8, dx)
+  }
+  for i := range data {
+      for j := range data[i] {
+          data[i][j] = uint8(i+j)
+      }
+  }
+  return data
 }
+
+func main() {
+	pic.Show(Pic)
+}
+
