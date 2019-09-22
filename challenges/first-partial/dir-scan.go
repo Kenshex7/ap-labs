9fdb575b5a836cfd4c40a02cf97ace59bf6e0d7f
Kenshex7 <hector12x13@gmail.com>
2019-09-22 13:32:03 -0500

    Examen primer parcial

--- challenges/first-partial/dir-scan.go
+++ challenges/first-partial/dir-scan.go
@@ -3,15 +3,60 @@ package main
 import (
 	"fmt"
 	"os"
+	"log"
+	"path/filepath"
 )
-
+type Content struct{
+	Dirs int
+	sLinks int
+	Devs int
+	Socks int
+	oFiles int
+}
 // scanDir stands for the directory scanning implementation
 func scanDir(dir string) error {
+	var Cont = Content{0,0,0,0,0}
+	
+
+	var eachArch=func(path string, infoX os.FileInfo, errX error) error{
+		fileType:= string(os.FileMode.String(infoX.Mode())[0])
+		//fmt.Println("El mode de este directorio es: ",fileType)
+		switch fileType{
+		case "d":
+			Cont.Dirs++
+		case "L":
+			Cont.sLinks++
+		case "S":
+			Cont.Socks++
+		case "D":
+			Cont.Devs++
+		default:
+			Cont.oFiles++
+		}
+		//fmt.Println("El nombre del archivo en el que entre es:",infoX.Name())
+		return nil
+	}
+	err:=filepath.Walk(dir,eachArch)
+	if err!=nil{
+		log.Fatal(err)
+	}
+	fmt.Println("Directory Scanner Tool")
+	fmt.Println("+---------------+-------+")
+	fmt.Println("|Path\t\t|",dir,"\t|")
+	fmt.Println("+---------------+-------+")
+	fmt.Println("|Directories\t|",Cont.Dirs,"\t|")
+	fmt.Println("|Symbolic links\t|",Cont.sLinks,"\t|")
+	fmt.Println("|Devices\t|",Cont.Devs,"\t|")
+	fmt.Println("|Sockets\t|",Cont.Socks,"\t|")
+	fmt.Println("|Other files\t|",Cont.oFiles,"\t|")
+	fmt.Println("+---------------+-------+")
+
+
+
 	return nil
 }
 
 func main() {
-
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: ./dir-scan <directory>")
 		os.Exit(1)
@@ -19,3 +64,4 @@ func main() {
 
 	scanDir(os.Args[1])
 }
+
--- challenges/first-partial/pacman-analyzer.c
+++ challenges/first-partial/pacman-analyzer.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 #define REPORT_FILE "packages_report.txt"
 
+typedef struct{
+	char name[50];
+	char instDate[50];
+	char upgrDate[50];
+    int upgrNum;
+	char remvDate[50];
+}package;
+
 void analizeLog(char *logFile, char *report);
+
 
 int main(int argc, char **argv) {
 
@@ -18,8 +28,118 @@ int main(int argc, char **argv) {
 
 void analizeLog(char *logFile, char *report) {
     printf("Generating Report from: [%s] log file\n", logFile);
-
+    package Packages[1000];
+    int j;
+    char markup[50];
+    for (j = 0; j < 1000; ++j)
+    {
+        Packages[j].name[0]='\0'; 
+        Packages[j].instDate[0]='\0';
+        Packages[j].upgrDate[0]='\0';
+        Packages[j].remvDate[0]='\0';
+    }
+    int i=0;
     // Implement your solution here.
+    int pInst=0,pUpgr=0,pRemv=0,pCurr=0;
+    FILE *origen;
+    FILE *destino;
+    origen=fopen(logFile,"r");
+    destino=fopen(report,"w");
+    char buff[1000];
+    char tmpDate[50];
+    char *line;
+    while(fgets(buff,300,(FILE*)origen)){
+    	if(strstr(buff,"installed")!=NULL){
+            line = strtok(buff," ");
+            line++;
+            strcpy(Packages[i].instDate,line);
+            strcpy(Packages[i].upgrDate,line);
+            //printf("package no: %d\n", i);
+            //printf("El nombre del elemento de la linea es -%s-\n", line);
+            line=strtok(NULL,"]");
+            strcat(Packages[i].instDate," ");
+            strcat(Packages[i].upgrDate," ");
+            //printf("El nombre del siguiente elemento de la linea es -%s-\n", line);
+            strcat(Packages[i].instDate,line);
+            strcat(Packages[i].upgrDate,line);
+            line=strtok(NULL," ");
+            line=strtok(NULL," ");
+            line=strtok(NULL," ");
+            strcpy(Packages[i].name,line);
+            //printf("El nombre del elemento de la linea es %s\n", line);
+            pInst++;
+            i++;
+    	}
+    	else if(strstr(buff,"upgraded")!=NULL){
+            line = strtok(buff," ");
+            line++;
+            strcpy(tmpDate,line);
+            line=strtok(NULL,"]");
+            strcat(tmpDate," ");
+            strcat(tmpDate,line);
+            line=strtok(NULL," ");
+            line=strtok(NULL," ");
+            line=strtok(NULL," ");
+            //Hasta aqui encuentra el nombre
+    		strcpy(markup,line);
+            j=0;
+            while((strcmp(markup,Packages[j].name))){
+                j++;
+            }
+            strcpy(Packages[j].upgrDate,tmpDate);
+            Packages[j].upgrNum++;
+            pUpgr++;
+    	}
+    	else if(strstr(buff,"removed")!=NULL){
+            line = strtok(buff," ");
+            line++;
+            strcpy(tmpDate,line);
+            line=strtok(NULL,"]");
+            strcat(tmpDate," ");
+            strcat(tmpDate,line);
+            line=strtok(NULL," ");
+            line=strtok(NULL," ");
+            line=strtok(NULL," ");
+            //Hasta aqui encuentra el nombre
+            strcpy(markup,line);
+            j=0;
+            while((strcmp(markup,Packages[j].name))){
+                j++;
+            }
+            strcpy(Packages[j].remvDate,tmpDate);
+    		pRemv++;
+    	}
+    	memset(buff,' ',1000);
+    }
+
+    pCurr=pInst-pRemv;
+    fprintf(destino,"Pacman Packages Report\n");
+    fprintf(destino,"----------------------\n");
+    fprintf(destino,"- Installed Packages\t: %d\n", pInst);
+    fprintf(destino,"- Removed Packages\t: %d\n", pRemv);
+    fprintf(destino,"- Upgraded Packages\t: %d\n", pUpgr);
+    fprintf(destino,"- Current Installed\t: %d\n\n", pCurr);
+    fprintf(destino,"List Of Packages\n");
+    fprintf(destino,"----------------\n");
+    i=0;
+    while(Packages[i].name[0]!='\0'){
+    	fprintf(destino,"-Package Name\t: %s\n", Packages[i].name);
+    	fprintf(destino,"   -Install Date\t: %s\n", Packages[i].instDate);
+    	fprintf(destino,"   -Last Update Date\t: %s\n", Packages[i].upgrDate);
+    	fprintf(destino,"   -How Many Updates\t: %d\n", Packages[i].upgrNum);
+    	if(Packages[i].remvDate[0]!='\0'){
+    		fprintf(destino,"   -Removal Date\t: %s\n", Packages[i].remvDate);
+    	}
+    	else{
+    		fprintf(destino,"   -Removal Date\t: -\n");
+    	}
+    	i++;
+        
+    }
+
+    fclose(origen);
+    fclose(destino);
 
     printf("Report is generated at: [%s]\n", report);
 }
+
